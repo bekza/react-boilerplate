8ee1a140989595ef505c588b1b33fc28ebe84a21
Bekzat Omrzakov <bekzat.omrzakov@walmartlabs.com>
2018-10-26 11:07:14 -0700

    clean up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import UserInput from './components/UserInput';
-import UserOutput from './components/UserOutput';
 
 class App extends Component {
-  state = {
-    username: "Bekzatik"
-  }
-
-  usernameChangeHandler = (newName) => {
-    this.setState({username: newName.target.value})
-  }
 
   render() {
     return (
       <div className="App">
-        <p> User Input </p>
-        <UserInput 
-          name={this.state.username}
-          usernameChange={this.usernameChangeHandler.bind(this)}/>
-        <p>User Output</p>
-        <UserOutput userName={this.state.username}/>
+        welcome
       </div>
     );
   }
